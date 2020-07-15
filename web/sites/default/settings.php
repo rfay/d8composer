adf87701cd14da48246fe80847d52336f72c5bc4
Randy Fay <randy@randyfay.com>
2020-07-15 13:57:05 -0600

    Add drush

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1,4 +1,5 @@
 <?php
+$settings['update_free_access'] = true;
 
 // #ddev-generated: Automatically generated Drupal settings file.
 if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
